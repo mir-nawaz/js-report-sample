6d2c48983a128733f6ca05fcdbf6677b500f3f57
Mir Nawaz <inbox.mirnawaz@gmail.com>
2019-04-18 17:07:18 +0500

    minor fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const reportingApp = express();
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (NODE_ENV !== 'development') {
+if (NODE_ENV === 'development') {
   app.use('/reporting', reportingApp);
 }
 
